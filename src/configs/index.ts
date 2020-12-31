f51e8c9f932b4f35094951784d9630a8292cd068
Elizeu Louzeiro <elizeumatheus16@gmail.com>
2020-12-30 23:05:33 -0300

    feat: created file to handle with all project configs enums

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,3 +1,5 @@
 export default {
-  ServerPort: process.env.PORT,
+  ConnectionString: process.env.MONGO_ATLAS_CONNECTION_STRING,
+  JWTSecret: process.env.APP_SECRET_KEY,
+  ServerPort: process.env.EXPRESS_PORT,
 }
